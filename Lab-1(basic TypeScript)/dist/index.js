a7771f5fb46a8be771c581442bc6357926fc1c4f
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-03-20 16:29:37 +0600

    basic typescript

---
+++ Lab-1(basic TypeScript)/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let age = 20;
+if (age >= 18) {
+    age += 10;
+}
+else {
+    age -= 10;
+}
+//# sourceMappingURL=index.js.map
---
+++ Lab-1(basic TypeScript)/src/index.ts
@@ -0,0 +1,7 @@
+let age:number = 20;
+if (age >= 18) {
+    age+=10;
+}
+else {
+    age-=10;
+}
